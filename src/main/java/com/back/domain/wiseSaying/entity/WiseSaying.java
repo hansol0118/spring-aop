dd816a61358596bcd7b03a4862ab2c3cfcf594d7
hansol0118 <wall197944@naver.com>
2026-03-04 15:24:59 +0900

    22-Repository 계층 추가

--- src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
@@ -4,17 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-
 @AllArgsConstructor
-@Setter
 @Getter
 public class WiseSaying {
+
+    @Setter
     private int id;
     private String content;
     private String author;
 
-    public void update(String content,String author){
-        this.content=content;
-        this.author=author;
+    public WiseSaying(String content, String author) {
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String content, String author) {
+        this.content = content;
+        this.author = author;
+    }
+
+    public boolean isNew() {
+        return this.id == 0;
     }
 }
